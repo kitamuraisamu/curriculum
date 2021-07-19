a4ecaa435859e3c80c42d61cb1a8ce80c04c546c
kitamuraisamu <iksubmail@gmail.com>
2021-07-20 08:58:42 +0900

    Create result.php
    いろいろなフォームの機能、課題

---
+++ 2-12/result.php
@@ -0,0 +1,10 @@
+<?php 
+    $your_name = $_POST['your_name'];
+    $gift = $_POST['gift'];
+    $number = $_POST['number'];
+?>
+    <p>お名前<?= $your_name;?>です。</p>
+    <p>ご希望景品：<?= $gift;?>です。</p>
+    <p>個数<?= $number;?>です。</p>
+
+
