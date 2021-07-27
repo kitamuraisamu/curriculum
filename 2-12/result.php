d8dd4fcd7645a4032a3a8026f9afc95b76f87f4f
kitamuraisamu <iksubmail@gmail.com>
2021-07-27 14:26:40 +0900

    Update result.php

--- 2-12/result.php
+++ 2-12/result.php
@@ -3,8 +3,9 @@
     $gift = $_POST['gift'];
     $number = $_POST['number'];
 ?>
-    <p>お名前<?= $your_name;?>です。</p>
-    <p>ご希望景品：<?= $gift;?>です。</p>
-    <p>個数<?= $number;?>です。</p>
+    <p>お名前：<?= $your_name;?></p>
+    <p>ご希望景品：<?= $gift;?></p>
+    <p>個数：<?= $number;?></p>
 
 
+s
