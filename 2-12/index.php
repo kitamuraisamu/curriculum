1914599b3f31cb2a35551c59c2162a08d6d5f4be
kitamuraisamu <iksubmail@gmail.com>
2021-07-27 14:26:30 +0900

    Update index.php

--- 2-12/index.php
+++ 2-12/index.php
@@ -4,16 +4,10 @@
     <input type="radio" name="gift" value="Ｂ賞" />Ｂ賞
     <input type="radio" name="gift" value="Ｃ賞" />Ｃ賞<br>
     個数：<select name="number">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
+        <?php 
+        for ($num=0;$num<=10;$num++) { 
+            ?> <option value="<?php echo $num ?>"><?php echo $num; ?></option>
+         <?php } ?>
         </select><br>
         <input type="submit" value="申込" />
 </form>
